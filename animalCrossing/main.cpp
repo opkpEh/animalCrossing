2deea8da53c6a3aa16f3b51664d19b82cb6d63b7
opkpEh <harshpandeykp20@gmail.com>
2023-10-01 20:35:03 +0530

    basic character movement added

--- animalCrossing/main.cpp
+++ animalCrossing/main.cpp
@@ -2,20 +2,54 @@
 
 class Game {
 public:
-    Game() {
-        screenWidth = 1000;
-        screenHeight = 480;
 
+    Texture2D grassSprite, playerSprite;
+    Rectangle playerSrc, playerDest;
+    float playerSpeed=2.25;
+
+    Game() {
+       
         InitWindow(screenWidth, screenHeight, "Animal Crossing");
         SetWindowState(FLAG_VSYNC_HINT);
         SetExitKey(0);
+
+        grassSprite = LoadTexture("res/Tilesets/Grass.png");
+        playerSprite = LoadTexture("res/Characters/BasicCharakterSpritesheet.png");
+
+        playerSrc = Rectangle{ 0, 0, 48, 48 };
+        playerDest = Rectangle{ 200, 200, 100, 100 };
+      
     }
 
-    void drawScene() {}
+    void drawScene()
+    {
+        DrawTexture(grassSprite, 100, 50, WHITE);
+        DrawTexturePro(playerSprite, playerSrc, playerDest, Vector2{ playerDest.width, playerDest.height }, 0, WHITE);
+    }
+    
 
-    void input() {}
+    void input()
+    {
+        if (IsKeyDown(KEY_W) || IsKeyDown(KEY_UP))
+        {
+            playerDest.y -= playerSpeed;
+        }
+        if (IsKeyDown(KEY_S) || IsKeyDown(KEY_DOWN))
+        {
+            playerDest.y += playerSpeed;
+        }
+        if (IsKeyDown(KEY_D) || IsKeyDown(KEY_RIGHT))
+        {
+            playerDest.x += playerSpeed;
+        }
+        if (IsKeyDown(KEY_A) || IsKeyDown(KEY_LEFT))
+        {
+            playerDest.x -= playerSpeed;
+        }
 
-    void update() {
+    }
+    void update() 
+    {
         running = !WindowShouldClose();
     }
 
@@ -30,6 +64,8 @@ public:
 
     void quit() {
         CloseWindow();
+        UnloadTexture(grassSprite);
+        UnloadTexture(playerSprite);
     }   
 
     bool isRunning() const {
@@ -37,8 +73,8 @@ public:
     }
 
 private:
-    int screenWidth;
-    int screenHeight;
+    const int screenWidth = 1000;
+    const int screenHeight = 480;
     bool running = true;
     Color bkgColor = { 147, 211, 196, 255 };
 };
